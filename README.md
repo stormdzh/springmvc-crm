fd6d57ced7ff3c702cd770cd025650576dbedbf7
stormdzh <460675145@qq.com>
2022-03-13 11:30:04 +0800

    添加忽略文件

---
+++ README.md
@@ -0,0 +1,12 @@
+项目来源：https://github.com/SongYu-max/-crm-/blob/master/crm%E6%95%B0%E6%8D%AE%E5%BA%93-20210420.sql
+
+项目存在问题：
+不能使用tomcat 7.0版本，需要使用9.0版本（切记）
+
+
+
+https://blog.csdn.net/cmjimmy/article/details/108355374
+
+
+https://blog.csdn.net/weixin_43805551/article/details/123226814
+
--- src/main/java/com/bjpowernode/crm/settings/service/impl/DicServiceImpl.java
+++ src/main/java/com/bjpowernode/crm/settings/service/impl/DicServiceImpl.java
@@ -19,21 +19,24 @@ public class DicServiceImpl implements DicService {
     public Map<String, List<DicValue>> getAll() {
 
         Map<String,List<DicValue>> map = new HashMap<String, List<DicValue>>();
+        try {
 
-        //将字典类型列表取出
-        List<DicType> dtList = dicTypeDao.getTypeList();
+            //将字典类型列表取出
+            List<DicType> dtList = dicTypeDao.getTypeList();
 
-        //将字段类型列表遍历
-        for (DicType dt:dtList){
-            //取得每一种类型的字典类型编码
-            String code = dt.getCode();
+            //将字段类型列表遍历
+            for (DicType dt : dtList) {
+                //取得每一种类型的字典类型编码
+                String code = dt.getCode();
 
-            //根据每一个字典类型来取得字典值列表
-            List<DicValue> dvList = dicValueDao.getListByCode(code);
-            map.put(code+"List",dvList);
+                //根据每一个字典类型来取得字典值列表
+                List<DicValue> dvList = dicValueDao.getListByCode(code);
+                map.put(code + "List", dvList);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-
         return map;
     }
 }
